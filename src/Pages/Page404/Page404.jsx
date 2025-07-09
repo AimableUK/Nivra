696de4eb8c87825a5bf5faa935a768deb641b2ca
UKOBIZABA Aimable <154277022+AimableTrague@users.noreply.github.com>
2025-07-09 12:11:16 +0200

    Handled Image, Data Usage Consent on The photos through avoiding all user drag events

--- src/Pages/Page404/Page404.jsx
+++ src/Pages/Page404/Page404.jsx
@@ -7,7 +7,7 @@ const Page404 = () => {
     <div className="absolute inset-0 flex flex-col justify-center items-center z-50">
       <div className="flex flex-row justify-between items-center">
         <img src={Nivra} alt="Nivra Logo" className="size-16" />
-        <h4 className="font-semibold text-lg md:text-2xl">PAGE NOT FOUND</h4>
+        <h4 className="font-semibold text-lg md:text-2xl text-slate-700">PAGE NOT FOUND</h4>
       </div>
       <span class="loader notFound"></span>
       <h4 className="absolute font-semibold text-lg md:text-2xl text-[#0870b7] -mt-10 -ml-2">
